66f623cb028706f27916036de72cb5fe4882a984
Andreas Philippi <andreas.philippi@cubus.ro>
2019-06-06 14:48:35 +0300

    Update application.js
    add no_asset_compression to comments

--- app/assets/javascripts/graphiql/rails/application.js
+++ app/assets/javascripts/graphiql/rails/application.js
@@ -1,3 +1,13 @@
+/**
+ * DO NOT REMOVE THIS COMMENT
+ * It turns off compression of the Konacha specs
+ * during the asset precompilation phase, to avoid
+ * spec failures caused by unwanted side effects
+ * of compression.
+ *
+ * no_asset_compression
+ *
+ **/
 //= require ./react-16.7.0
 //= require ./react-dom-16.7.0
 //= require ./fetch-0.10.1
